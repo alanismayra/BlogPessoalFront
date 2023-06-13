91bdfff75c7ab864a9a053bee3fe5944beb43fb3
Alanis Mayra <alanismayra5000@gmail.com>
2023-06-13 15:27:24 -0300

    implementando o react no blog pessoal

---
+++ blogpessoal/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+
+function Home() {
+return(
+    <>
+    <h1 className="titulo">Home</h1>
+    <img src='./images/vesgo1.jpg' alt="imagem do meu gatinho" className="img"/>
+    </>
+)
+}
+
+export default Home;
