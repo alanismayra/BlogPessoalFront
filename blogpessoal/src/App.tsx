d9d88c083c73db3870fa116527e867dff249fd0e
Alanis Mayra <alanismayra5000@gmail.com>
2023-06-14 18:58:39 -0300

    conhecendo material ui, grid e flexBox

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -3,13 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/home/Home';
+import { Grid } from '@material-ui/core';
 
 function App() {
 
   return (
-    <Home/>
+    <>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={12}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Home />
+      </Grid>
+
+    </Grid>
+
+    </>
      
   )
 }
 
-export default App
+export default App;
--- blogpessoal/src/pages/home/Home.tsx
+++ blogpessoal/src/pages/home/Home.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import {Button, Paper, Box} from '@material-ui/core'
 import './Home.css';
-
 
 function Home() {
 return(
     <>
-    <h1 className="titulo">Home</h1>
-    <img src='./images/vesgo1.jpg' alt="imagem do meu gatinho" className="img"/>
+    <Paper>
+        <Box p={2}>
+            <Box display="flex" justifyContent="center">
+                <h1>Título</h1>
+            </Box>
+            <img src="./images/vesgo2.jpg" alt="" style={{width:"100%", height:"100%"}} />
+            <Box display="flex" justifyContent="center" p={2}>
+                <Button variant="contained" color="primary"> Vesgueiro </Button>
+                <Button variant="contained" color="secondary"> Vesgueiro </Button>
+            </Box>
+        </Box>
+    </Paper>
     </>
 )
 }
