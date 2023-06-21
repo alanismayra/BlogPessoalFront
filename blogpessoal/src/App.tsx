196512bb26c68906315623f5edc6bb3cdd871bfe
Alanis Mayra <alanismayra5000@gmail.com>
2023-06-20 22:40:20 -0300

    incluindo o cadastrar e login do usuario

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import './App.css'
 
 
@@ -17,7 +18,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+          <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
